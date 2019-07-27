28d26c267ea246de79ee776d428db586520fb5a3
Martín Nieva <martinjafactor@gmail.com>
2019-07-27 13:41:56 -0300

    Added toggle movement and improved algorithm

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from curses import textpad
 
 def main(stdscr):
     curses.curs_set(0)
+    stdscr.nodelay(1)
+    stdscr.timeout(1000 // 5)
 
     screen_height, screen_width = stdscr.getmaxyx()
 
@@ -12,7 +14,8 @@ def main(stdscr):
         [screen_height // 2, screen_width // 2],
         [screen_height // 2, screen_width // 2 - 1],
         [screen_height // 2, screen_width // 2 - 2],
-        [screen_height // 2, screen_width // 2 - 3]
+        [screen_height // 2, screen_width // 2 - 3],
+        [screen_height // 2, screen_width // 2 - 4],
     ]
 
     new_head = list()
@@ -20,25 +23,33 @@ def main(stdscr):
     for body_part in snake_body:
         stdscr.addch(body_part[0], body_part[1], "#")
 
+    direction = (1, 1)
+
     while True:
+#        stdscr.addstr(0, 0, "direction = {}".format(direction))
+#        stdscr.addstr(1, 0, "new_head = {}".format(new_head))
+#        stdscr.addstr(2, 0, "snake_body = {}".format(snake_body))
+
         key = stdscr.getch() 
 
         if key == curses.KEY_RIGHT:
-            new_head = [snake_body[0][0], snake_body[0][1] + 1]
+            direction = (1, 1)
 
         elif key == curses.KEY_LEFT:
-            new_head = [snake_body[0][0], snake_body[0][1] - 1]
+            direction = (1, -1)
 
         elif key == curses.KEY_DOWN:
-            new_head = [snake_body[0][0] + 1, snake_body[0][1]]
+            direction = (0, 1)
 
         elif key == curses.KEY_UP:
-            new_head = [snake_body[0][0] - 1, snake_body[0][1]]
-        
+            direction = (0, -1)
+
+        new_head = snake_body[0].copy()
+        new_head[direction[0]] += direction[1]
+
         snake_body.insert(0, new_head)
 
         stdscr.addch(snake_body[0][0], snake_body[0][1], "#")
-
         stdscr.addch(snake_body[-1][0], snake_body[-1][1], " ")
         
         snake_body.pop()
