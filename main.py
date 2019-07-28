2a361409cc25a0fc414e4726578ce3d2339ae410
Martín Nieva <martinjafactor@gmail.com>
2019-07-28 17:51:53 -0300

    The player wont quit when going his opposite direction anymore

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def main(stdscr):
     rect_top_left = (3, screen_width // 3)
     rect_bottom_right = (screen_height - 3, (screen_width // 3) * 2)
 
+    # Creating the box in which the player will be able to move
     textpad.rectangle(
         stdscr, 
         rect_top_left[Y], 
@@ -22,6 +23,7 @@ def main(stdscr):
         rect_bottom_right[X]
     )
 
+    # Creating the snake (first parameter is the length)
     snake = Snake(
         4, 
         screen_height, 
@@ -29,6 +31,7 @@ def main(stdscr):
         (HORIZONTAL, VELOCITY)
     )
 
+    # Initializing the food
     food = Food(
         rect_top_left[Y], 
         rect_top_left[X], 
@@ -45,26 +48,27 @@ def main(stdscr):
     for body_part in snake.body:
         stdscr.addch(body_part[Y], body_part[X], "#")
 
-    stdscr.addstr(2, rect_top_left[X], "Length of your snake: ")
+    stdscr.addstr(2, rect_top_left[X], "Length of your snake: ") # UI element
 
-    stdscr.addstr(screen_height - 1, screen_width // 2 - len("Press ESC to quit") // 2, "Press ESC to quit")
+    stdscr.addstr(screen_height - 1, screen_width // 2 - len("Press ESC to quit") // 2, "Press ESC to quit") # UI element
 
     while True:
         key = stdscr.getch() 
 
-        if key == curses.KEY_RIGHT:
+        # If statements for movement
+        if key == curses.KEY_RIGHT and snake.direction != (HORIZONTAL, -VELOCITY):
             snake.direction = (HORIZONTAL, VELOCITY)
 
-        elif key == curses.KEY_LEFT:
+        elif key == curses.KEY_LEFT and snake.direction != (HORIZONTAL, VELOCITY):
             snake.direction = (HORIZONTAL, -VELOCITY)
 
-        elif key == curses.KEY_DOWN:
+        elif key == curses.KEY_DOWN and snake.direction != (VERTICAL, -VELOCITY):
             snake.direction = (VERTICAL, VELOCITY)
 
-        elif key == curses.KEY_UP:
+        elif key == curses.KEY_UP and snake.direction != (VERTICAL, VELOCITY):
             snake.direction = (VERTICAL, -VELOCITY)
 
-        elif key == 27:
+        elif key == 27: # If player presses ESC, then quit
             break
 
         position, delta = snake.direction
@@ -72,18 +76,21 @@ def main(stdscr):
         new_head = snake.body[HEAD].copy()
         new_head[position] += delta
 
-        if new_head[Y] == rect_top_left[Y] or new_head[Y] == rect_bottom_right[Y]:
+        # Make the player quit if he touches the walls
+        if new_head[Y] == rect_top_left[Y] or new_head[Y] == rect_bottom_right[Y]: 
             break
 
         elif new_head[X] == rect_top_left[X] or new_head[X] == rect_bottom_right[X]:
             break
    
-        if new_head in snake.body:
-            break
-
+        # Make the player's length increment if he touches the drawn food 
         if new_head[Y] == food.y and new_head[X] == food.x:
             snake.length += 1
             food.create_and_draw(stdscr, snake.body)
+
+        # Make the player quit if he touches himself
+        if new_head in snake.body:
+            break
 
         snake.body.insert(0, new_head)
 
@@ -97,4 +104,7 @@ def main(stdscr):
 
         stdscr.addstr(2, rect_bottom_right[X] - (snake_length_digits - 1), "{}".format(snake.length))
 
+        # stdscr.addstr(screen_height - 6, 5, "{}".format(snake.body))
+        # stdscr.addstr(screen_height - 1, 5, "{} {}".format(food.y, food.x))
+    
 curses.wrapper(main)
